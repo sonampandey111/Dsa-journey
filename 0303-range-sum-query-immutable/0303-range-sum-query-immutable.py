d13c778a0b476e6478509a9a6d8c115044643a40
sonam <sp9336773@gmail.com>
2026-08-21 10:38:06 +0530

    Time: 2617 ms (12.82%), Space: 22.6 MB (79.43%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -0,0 +1,18 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums=nums
+        
+
+    def sumRange(self, left: int, right: int) -> int:
+        total=0
+        for i in range(left,right+1):
+
+            total+=self.nums[i]
+        return total    
+        
+
+# Your Numy object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
+
